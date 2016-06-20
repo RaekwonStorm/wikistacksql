11574dc7e8db879eae3b6666cf92e13f5db8700b
Reed Branson <reed.branson@gmail.com>
2016-06-20 12:58:47 -0400

    lunch time

--- routes/wiki.js
+++ routes/wiki.js
@@ -1,13 +1,31 @@
 var express = require('express');
 var router = express.Router();
+var models = require('../models');
+var Page = models.Page;
+var User = models.User;
 
 
 router.get('/', function(req, res){
-  res.render('index');
+  res.redirect('/');
 });
 
 router.post('/', function(req, res){
-  res.send('post worked');
+  var page = Page.build({
+    title: req.body.title,
+    content: req.body.content,
+    status: req.body.status
+  });
+
+  var user = User.build({
+    name: req.body.name,
+    email: req.body.email
+  });
+
+  page.save();
+  user.save();
+
+  res.redirect('/');
+
 });
 
 router.get('/add/', function(req, res){
